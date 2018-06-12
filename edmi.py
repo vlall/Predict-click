13fdb9809ef3094965acc6b6899d654016dedc40
Mela Hardin <39716585+melahardin@users.noreply.github.com>
2018-06-12 13:35:08 -0400

    Update edmi.py

--- edmi.py
+++ edmi.py
@@ -1,5 +1,27 @@
+#!/usr/bin/env python
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 import pandas as pd  #imports the pandas library and aliasing as pd
 import re
+
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
 import numpy as np
 import json
 import csv  #converting new_list to csv file
@@ -12,13 +34,24 @@ df.to_csv('test_edmi_10k.csv')
 condensed_10k = df.loc[:, ['USERNAME', 'SOURCE_APP', 'SOURCE_APP_FUNCTION']]
 condensed_10k.to_csv('condensed_10k.csv', index=False)
 
-# filter out NaN items from column 'SOURCE_APP_FUNCTION' and call it app_func
+# filter out NaN items from column 'SOURCE_APP_FUNCTION'
 df = df[pd.notnull(df['SOURCE_APP_FUNCTION'])]
 
 # get items with non-alphanumeric pattern, r for raw string and the + for repeat
 pattern = re.compile(r'\W+')
 
+stop_words = set(stopwords.words('english'))
+word_tokens = word_tokenize(df['SOURCE_APP_FUNCTIONS'])
+item_filtered = [item for item in word_tokens if item not in stop_words]
 for item in df['SOURCE_APP_FUNCTION']:
+
+    # sub non-alphanumeric pattern with a space
     item_new = re.sub(pattern, ' ', item)
-    print(item_new)
+
+    if item_new not in stop_words:
+        item_filtered.append(item_new)
+        print(item_new)
+
+# still working on this...
+
 
